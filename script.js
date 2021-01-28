d8da70e2d6f479773d162f65278dac86272658b3
Hayde <49920514+h4yde@users.noreply.github.com>
2021-01-28 00:02:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var killComments = setInterval(function(){ 
 	if($$("button[id*='overflow-menu']")[0] == undefined) clearInterval(killComments);
 	else {
-		$$("button[id*='overflow-menu']")[2].click();
+		$$("button[id*='overflow-menu']")[0].click();
 		$$("button[role='menuitem']")[3].click();
-		$$("button[role='button']")[4].click();
+		$$("footer button")[1].click();
 	}
 }, 10);
